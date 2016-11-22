4f602db165a67a51d3b7e8ad373071f66d3a346d
gaothing <74498745@qq.com>
2016-11-22 23:15:46 +0800

    update inreading.js

--- reading/inreading/inreading.js
+++ reading/inreading/inreading.js
@@ -35,68 +35,47 @@ app.directive("myreading",[function(){
 				var first;
 	var inNub=0;
 	
-//	$(el).find(".set li:nth-child(1)").on("click",function(){
-//		console.log(0)
-//		$(el).find(".page-opacity").removeClass("ftstact").addClass("ftstact2");
-//		$(".font-set").removeClass("ftstact2").addClass("ftstact")
-//	})
-//	$(el).find(".set li:nth-child(3)").on("click",function(){
-//		console.log(0)
-//		$(el).find(".page-opacity").removeClass("ftstact").addClass("ftstact2");
-//		$(el).find(".back-op").removeClass("ftstact2").addClass("ftstact")
-//	})
-////	读书页面滑出菜单
-//	$(el).find("a").on("touchstart",false);
-//console.log($(el).find(".nav a"))
-//$(el).find(".nav a").on("click",function(){
-//	console.log(9)
-//})
-//	$(el).find(".out-by").on("touchstart",false);
+
+	$(el).find(".out-by").on("touchstart",false);
 //	$(el).find(".page-opacity").on("touchstart",false);
 //	$(el).find(".header-eight").on("touchstart",false);
-//	$(el).find("#reading").on("touchstart",function(e){	
-//e.preventDefault();
-//
-//	if(e.originalEvent.changedTouches[0].clientY>600){
-//		 first=e.originalEvent.changedTouches[0].clientY;
-//		}
-//		if(inNub%2==0){
+	
+	$(el).on("click",function(){
+		if(inNub%2==0){
 			$(el).find(".out-by").addClass("out-act").removeClass("out-act2")
 			$(el).find(".header-eight").addClass("head-act").removeClass("head-act2")
+			if($(el).find(".back-op").hasClass("ftstact")){
+				$(el).find(".back-op").removeClass("ftstact").addClass("ftstact2")
+				nub+=1
+				return
+			}if($(el).find(".font-set").hasClass("font-act")){
+				$(el).find(".font-set").removeClass("font-act").addClass("font-act2")
+				nub+=1
+				return
+			}
+			
 			$(el).find(".page-opacity").removeClass("fontSet-act2").addClass("fontSet-act")
-//		}else{
-//			$(el).find(".header-eight").addClass("head-act2").removeClass("head-act");
-//			$(el).find(".out-by").addClass("out-act2").removeClass("out-act")
-//			$(el).find(".page-opacity").removeClass("fontSet-act").addClass("fontSet-act2")
-//
-//		}
-//		inNub+=1
-//		
-//	})
-	
-////	下方点击
-//	console.log($(".page-opacity .pro-go"))
-//	$(".page-opacity .pro-go").find(".pro-cir").on("touchstart",function(e){
-//		e.preventDefault();
-//		var proF=e.originalEvent.changedTouches[0].clientX;
-//		$(document).on("touchmove",function(e){
-//			var progo=e.originalEvent.changedTouches[0].clientX-$(".pro-cir").offset().left+($(".pro-cir").outerWidth()/2)
-//				if(progo>=0 && progo<466){
-//					$(".pro-cir").css("left",progo)
-//				}else{
-//					return
-//				}
-//				
-//			})
-//	})
-//$(document).on("touchend", function() {
-//				$(document).off("touchmove")
-//				$(document).off("touchend").touch
-//			})
+				}else{
+					if($(el).find(".back-op").hasClass("ftstact")){
+						  inNub+=1
+						return
+					}else if($(el).find(".font-set").hasClass("font-act")){
+						 inNub+=1
+						return 
+						
+					}
+					$(el).find(".header-eight").removeClass("head-act").addClass("head-act2");
+					$(el).find(".out-by").removeClass("out-act").addClass("out-act2")
+					$(el).find(".page-opacity").removeClass("fontSet-act").addClass("fontSet-act2")
+//			$(el).find(".font-set").css("display","none")
+				}
+				inNub+=1
+	})
 ////	調節字號大小
-//	$(el).find(".font-set .pro-go").find(".pro-cir").on("touchstart",function(e){
+//	$(el).find(".pro-cir").on("touchstart",function(e){
 //		e.preventDefault();
 //		var proF=e.originalEvent.changedTouches[0].clientX;
+//		console.log(proF)
 //		$(el).on("touchmove",function(e){
 //			var progo=e.originalEvent.changedTouches[0].clientX-$(el).find(".pro-cir").offset().left+($(el).find(".pro-cir").outerWidth()/2)
 //				if(progo>=0 && progo<466){
@@ -110,9 +89,9 @@ app.directive("myreading",[function(){
 //				$(el).off("touchmove")
 //				$(el).off("touchend")
 //			})
-
-
-
+//
+//
+//
 
 
 
@@ -136,10 +115,15 @@ $(el).find(".shezhi").on("click",function(){
 	$(el).find(".page-opacity").removeClass("fontSet-act").addClass("fontSet-act2");	$(el).find(".font-set").removeClass("font-act2").addClass("font-act")
 })
 $(el).find(".liang").on("click",function(){
-	$(el).find(".page-opacity").removeClass("fontSet-act").addClass("fontSet-act2");	$(el).find(".font-set").css("display","none")
+	$(el).find(".page-opacity").removeClass("fontSet-act").addClass("fontSet-act2");	$(el).find(".font-set").removeClass("font-act").addClass("font-act2")
 
 	$(el).find(".back-op").removeClass("ftstact2").addClass("ftstact")
 })
+//////////////////////////////////拖动///////////////////////////////////////
+
+
+
+
 		}
 	}
 }])
